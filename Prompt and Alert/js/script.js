cfeb6f4539582684973e5b6b17c97023611b6f10
twashi28 <terencewashington@Terences-MacBook-Pro.local>
2014-08-14 00:51:24 -0400

    Prompt and Alert
    Using Prompt and Alert to manipulate users input.

---
+++ Prompt and Alert/js/script.js
@@ -0,0 +1,10 @@
+//Prompt and Alert
+//var userInput = prompt("Enter your year of birth:");
+//console.log(userInput);
+
+var width = prompt("We are calculating the area of a triangle. \nPlease enter the width");
+var height = prompt("Please enter the height");
+var area = width * height; // calculates the info
+console.log(area);
+var result = "The area of your retangle is " + area + " ft. ";
+alert(result);
