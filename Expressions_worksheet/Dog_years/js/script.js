b81abf099cecec1b25551b37303b0e282bc88d09
twashi28 <terencewashington@Terences-MacBook-Pro.local>
2014-08-14 02:14:39 -0400

    Dog_years
    Calculating the human years from the dogs age.

---
+++ Expressions_worksheet/Dog_years/js/script.js
@@ -0,0 +1,14 @@
+// Terence Washington, August 14, 2014, Dog Years
+
+// The user can input the human years of the dog here
+var dogAge = prompt("How old is Sparky in human years? \n Age:");
+
+//The human age is calculated
+var humanAge = dogAge * 7;
+
+// This shows the result after the calculation
+var result = "Sparky is " + humanAge + " human years old which is " + dogAge + " in dog years.";
+alert(result);
+
+// Print the dog's age
+console.log(dogAge);
