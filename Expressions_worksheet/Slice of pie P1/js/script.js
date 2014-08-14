3c40303b2b7d55156094e03333764c427de14b33
twashi28 <terencewashington@Terences-MacBook-Pro.local>
2014-08-14 02:52:48 -0400

    Slice of pie part I
    Calculated how many slice for each person at the party

---
+++ Expressions_worksheet/Slice of pie P1/js/script.js
@@ -0,0 +1,19 @@
+// Terence Washington, August 14, 2014, Slice of Pie part 1
+
+// The number of slices
+var pSlice = 8;
+
+//the number of people at the party
+var students = 30;
+
+//The number of pizzas ordered
+var pizzas = 9;
+
+//The calculation of how many slices of pizza each person can have
+var eaten = pSlice * pizzas / students;
+
+//The result of the calculation
+var result = "Each person ate " + eaten + " slices of pizza at the party."
+
+//Print the result
+console.log(result);
