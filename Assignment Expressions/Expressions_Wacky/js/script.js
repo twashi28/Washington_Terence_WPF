75d9e1de463c65419980374e686991c3b968ad5d
twashi28 <terencewashington@Terences-MacBook-Pro.local>
2014-08-14 23:15:38 -0400

    Change adjust
    setup an array to start my wacky calculation

--- Assignment Expressions/Expressions_Wacky/js/script.js
+++ Assignment Expressions/Expressions_Wacky/js/script.js
@@ -1,1 +1,27 @@
-alert("This is in my js file");
+/*
+ * Terence Washington
+ * WPF Section 01
+ * Expressions Wacky
+ * 8-14-2014
+ */
+
+// setup an array of ranom numbers
+var random = [3, 45, 23, 89, 84, 466];
+
+// Print the inputs to the console
+console.log(lenght);
+console.log(width);
+console.log(height);
+
+
+//multiplies the input of the lenght, width, and height.
+var volume = Number(lenght) * Number(width) * Number(height);
+
+// Print the volume to the console
+console.log(volume);
+
+//set the result in a concatening string
+var result = "The volume of the warehouse building is " + volume + " cubic feet.";
+
+//alert the result back to the user
+alert(result);
