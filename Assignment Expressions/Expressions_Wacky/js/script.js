b22037d7d4354fc44fd5920fbdc21d3fbf916cc4
twashi28 <terencewashington@Terences-MacBook-Pro.local>
2014-08-14 23:25:04 -0400

    Finished prompts
    Put three prompts in and added console logs

--- Assignment Expressions/Expressions_Wacky/js/script.js
+++ Assignment Expressions/Expressions_Wacky/js/script.js
@@ -9,14 +9,16 @@
 var random = [3, 45, 23, 89, 84, 466];
 
 // Print the inputs to the console
-var day = prompt ("Type the number of the day of your birthday. \n 1 -  31");l
-var = prompt ("");
+var day = prompt ("Type the number of the day of your birthday. \n 1 -  31:");
+var clap = prompt ("How many times have yoou yawned today? \n Yawns:");
+var age = prompt ("How old are you today? \n age:")
 
 //multiplies the input of the lenght, width, and height.
 var volume = Number(lenght) * Number(width) * Number(height);
 
 // Print the volume to the console
-console.log(volume);
+console.log(day);
+console.log(clap);
 
 //set the result in a concatening string
 var result = "The volume of the warehouse building is " + volume + " cubic feet.";
