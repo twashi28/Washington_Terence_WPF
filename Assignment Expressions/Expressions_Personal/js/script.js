7292ab18c27643733e57aa382d618e2ea0626dbd
twashi28 <terencewashington@Terences-MacBook-Pro.local>
2014-08-14 20:36:25 -0400

    Expressions assignment
    declaring the variables

---
+++ Assignment Expressions/Expressions_Personal/js/script.js
@@ -0,0 +1,13 @@
+/*
+ * Terence Washington
+ * WPF Section 01
+ * Expressions Personal
+ * 8-14-2014
+ */
+
+// Inputing the hours of each day that have been played
+var monday = prompt("How many hours have you played Tetris for monday? \n Enter Hours:");
+var tuesday = prompt("How many hours have you played Tetris for tuesday? \n Enter Hours:");
+var wedsnesday = prompt("How many hours have you played Tetris for wedsnesday? \n Enter Hours:");
+var thursday = prompt("How many hours have you played Tetris for thursday? \n Enter Hours:");
+var friday = prompt("How many hours have you played Tetris for friday? \n Enter Hours:");
