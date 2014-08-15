a4432fb4221b9b6a86e41faa5799937609d119dc
twashi28 <terencewashington@Terences-MacBook-Pro.local>
2014-08-14 20:55:08 -0400

    Made personal added result
    modified the variables to make more personal and added the result and
    alert actions

--- Assignment Expressions/Expressions_Personal/js/script.js
+++ Assignment Expressions/Expressions_Personal/js/script.js
@@ -6,14 +6,16 @@
  */
 
 // Inputing the hours of each day that have been played
-var monday = prompt("How many hours have you played Tetris for monday? \n Enter Hours:");
-var tuesday = prompt("How many hours have you played Tetris for tuesday? \n Enter Hours:");
-var wedsnesday = prompt("How many hours have you played Tetris for wedsnesday? \n Enter Hours:");
-var thursday = prompt("How many hours have you played Tetris for thursday? \n Enter Hours:");
-var friday = prompt("How many hours have you played Tetris for friday? \n Enter Hours:");
+var monday = prompt("How many hours have I played Tetris for monday? \n Enter Hours:");
+var tuesday = prompt("How many hours have I played Tetris for tuesday? \n Enter Hours:");
+var wedsnesday = prompt("How many hours have I played Tetris for wedsnesday? \n Enter Hours:");
+var thursday = prompt("How many hours have I played Tetris for thursday? \n Enter Hours:");
+var friday = prompt("How many hours have I played Tetris for friday? \n Enter Hours:");
 
 //Find the average of the hours played for the week.
-var average = (monday + tuesday + wedsnesday + thursday + friday) / 5;
+var average = monday + tuesday + wedsnesday + thursday + friday / 5;
 
 //output the average of the week
 console.log(average);
+var result = "The average hours I have played tetris is " + average + " hrs.";
+alert(result);
