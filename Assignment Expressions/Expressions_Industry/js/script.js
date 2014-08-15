066d222b31f031371ab5cafc321e461beea6a1b3
twashi28 <terencewashington@Terences-MacBook-Pro.local>
2014-08-14 23:03:56 -0400

    Changes for next assignment
    I copies the last assignment and made changes to fit the new assignment

--- Assignment Expressions/Expressions_Industry/js/script.js
+++ Assignment Expressions/Expressions_Industry/js/script.js
@@ -1,1 +1,24 @@
-alert("This is in my js file");
+/*
+ * Terence Washington
+ * WPF Section 01
+ * Expressions Industry
+ * 8-14-2014
+ */
+
+// Inputing the lenght, width, and height of the warehouse building
+var lenght = prompt("How many hours have I played Tetris for monday? \n Enter Hours:");
+var width = prompt("How many hours have I played Tetris for tuesday? \n Enter Hours:");
+var height = prompt("How many hours have I played Tetris for wedsnesday? \n Enter Hours:");
+
+
+//multiplies the input of the lenght, width, and height.
+var volume = Number(lenght) * Number(width) * Number(height);
+
+// Print the volume to the console
+console.log(volume);
+
+//set the result in a concatening string
+var result = "The volume of the warehouse building is " + volume + " cubic feet.";
+
+//alert the result back to the user
+alert(result);
