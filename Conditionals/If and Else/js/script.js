8070bf86fe32d6cf302c6ae983ac4aad05cd6f6c
twashi28 <terencewashington@Terences-MacBook-Pro.local>
2014-08-19 02:33:09 -0400

    If and else
    Two If statements

---
+++ Conditionals/If and Else/js/script.js
@@ -0,0 +1,17 @@
+/* Terence Washington
+ * If and else
+ */
+
+var kidHeight = 50;
+var minHeight = 48;
+
+// If the child is over 48 in height
+
+if(kidHeight > minHeight){
+	// code performed if condition is true
+	console.log("You can ride the coaster.");
+}
+if(kidHeight < minHeight){
+	// code performed if condition is true
+	console.log("You are too short for this ride.");
+}
