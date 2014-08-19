4c96c867c159de23128f9f99c94a640bd5209362
twashi28 <terencewashington@Terences-MacBook-Pro.local>
2014-08-19 02:20:29 -0400

    Condition with Expression
    adding a value in the statement

---
+++ Conditionals/Condition with Expression/js/script.js
@@ -0,0 +1,14 @@
+/* Terence Washington
+ * Condition with Expression
+ */
+
+var kidHeight = 47;
+var minHeight = 48;
+
+// If the child is over 48 in height
+
+if(kidHeight+2 > minHeight){
+	// code performed if condition is true
+	console.log("You can ride the coaster.");
+}
+console.log("You can not ride");
