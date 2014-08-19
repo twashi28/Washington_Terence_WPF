96585da3e86d3a6d864366323e4ea77fd3b21b41
twashi28 <terencewashington@Terences-MacBook-Pro.local>
2014-08-19 04:51:27 -0400

    Logical Operators
    Two conditions but giving both answers. Need to add the AND (&&)
    operator.

---
+++ Conditionals/Logical Operators/js/script.js
@@ -0,0 +1,22 @@
+/* Terence Washington
+ * Logical Operators
+ */
+
+var budget = 300;
+var smartPhone = 199.99;
+var payCheck = 200;
+
+// if the price of the phone is less than the budget AND (&&) if paycheck is over 300
+if (smartPhone < budget){
+	console.log("We can get the phone");
+	
+}else{
+	console.log("We can not get the phone");
+	
+}
+if (payCheck > 300) {
+	console.log("We can get the phone");
+	
+}else{
+	console.log("We can not get the phone");
+};
