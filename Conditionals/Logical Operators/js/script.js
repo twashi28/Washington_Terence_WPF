4e0929a0e474715e58fc8cc7ce9d277dfaf8cf45
twashi28 <terencewashington@Terences-MacBook-Pro.local>
2014-08-19 04:54:08 -0400

    AND operator
    Removed the second if statement and combined the AND operator into the
    first if statement.

--- Conditionals/Logical Operators/js/script.js
+++ Conditionals/Logical Operators/js/script.js
@@ -7,16 +7,10 @@ var smartPhone = 199.99;
 var payCheck = 200;
 
 // if the price of the phone is less than the budget AND (&&) if paycheck is over 300
-if (smartPhone < budget){
+if (smartPhone < budget && payCheck > 300){
 	console.log("We can get the phone");
 	
 }else{
 	console.log("We can not get the phone");
 	
 }
-if (payCheck > 300) {
-	console.log("We can get the phone");
-	
-}else{
-	console.log("We can not get the phone");
-};
