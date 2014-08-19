5e9a97cb3a376102bb809ab40db3947df6b28aa0
twashi28 <terencewashington@Terences-MacBook-Pro.local>
2014-08-19 05:16:02 -0400

    OR || operator
    Rewritten the code to exercise the OR || operator

--- Conditionals/Logical Operators/js/script.js
+++ Conditionals/Logical Operators/js/script.js
@@ -2,12 +2,12 @@
  * Logical Operators
  */
 
-var budget = 300;
+var budget = 100;
 var smartPhone = 199.99;
-var payCheck = 200;
+var wonLottery = true;
 
 // if the price of the phone is less than the budget AND (&&) if paycheck is over 300
-if (smartPhone < budget && payCheck > 300){
+if (smartPhone < budget || wonLottery=== true){
 	console.log("We can get the phone");
 	
 }else{
