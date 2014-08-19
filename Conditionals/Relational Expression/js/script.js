ac5193af7f9d22195ccd70003164fd7ed3686980
twashi28 <terencewashington@Terences-MacBook-Pro.local>
2014-08-19 02:05:27 -0400

    Relational Expression
    Using kid height

---
+++ Conditionals/Relational Expression/js/script.js
@@ -0,0 +1,13 @@
+/* Terence Washington
+ * Relational Expression
+ */
+
+var kidHeight = 50;
+
+// If the child is over 48 in height
+
+if(kidHeight > 48){
+	// code performed if condition is true
+	console.log("You can ride the coaster.");
+}
+console.log("You can not ride");
