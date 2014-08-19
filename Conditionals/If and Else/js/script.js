650709d73d7ff17d39842091e43a66fbd1858328
twashi28 <terencewashington@Terences-MacBook-Pro.local>
2014-08-19 02:40:49 -0400

    else statement
    Replaced second if statement with an else statement.

--- Conditionals/If and Else/js/script.js
+++ Conditionals/If and Else/js/script.js
@@ -2,7 +2,7 @@
  * If and else
  */
 
-var kidHeight = 48;
+var kidHeight = 50;
 var minHeight = 48;
 
 // If the child is over 48 in height
@@ -10,8 +10,7 @@ var minHeight = 48;
 if(kidHeight > minHeight){
 	// code performed if condition is true
 	console.log("You can ride the coaster.");
-}
-if(kidHeight <= minHeight){
-	// code performed if condition is true
+}else{
+	// code performed if condition is false
 	console.log("You are too short for this ride.");
 }
