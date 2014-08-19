d426647a5e5441cd90c4f1bd7e54f7fc3879a102
twashi28 <terencewashington@Terences-MacBook-Pro.local>
2014-08-19 05:51:19 -0400

    Ternary opeator
    after the ternary operator

--- Conditionals/Ternary Opereators/js/script.js
+++ Conditionals/Ternary Opereators/js/script.js
@@ -4,14 +4,10 @@
 
 
 
-var age = 11;
+var age = 6;
 var book;
 
 // if the child is under 10, they get green eggs and ham, otherwise they get the time machine
 
-if(age <10){
-	book = "Green Eggs and Ham";
-}else{
-	book = "Time Machine";
-}
+book = (age < 10) ? "Green Eggs and Ham" : "Time Machine";
 console.log(book);
