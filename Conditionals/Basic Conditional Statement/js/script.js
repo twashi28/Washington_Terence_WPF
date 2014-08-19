958cfa3d644a907c71639180ad5e1a2a291adccb
twashi28 <terencewashington@Terences-MacBook-Pro.local>
2014-08-19 01:49:06 -0400

    Conditional Statement
    Basic Conditional Statement

---
+++ Conditionals/Basic Conditional Statement/js/script.js
@@ -0,0 +1,13 @@
+/* Terence Washington
+ * Conditional logic
+ */
+
+var oldEnough = false;
+
+// If the child is old enough, print to console "you can ride!"
+
+if(oldEnough){
+	// code performed if condition is true
+	console.log("You can ride the coaster.");
+}
+console.log("What comes after?");
