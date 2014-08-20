1803ddf45e81ce2cbf2c66cfdb5dd9360315ec8a
twashi28 <terencewashington@Terences-MacBook-Pro.local>
2014-08-19 22:42:52 -0400

    Adjust
    adjusting the calculations

--- Conditionals_worksheet/Group 1/js/script.js
+++ Conditionals_worksheet/Group 1/js/script.js
@@ -7,15 +7,16 @@
 
 // Setting the variables and prompts
 var degree = prompt ("What degree would you like to convert to \n C or F:");
-var temp = Number(prompt ("What is the temperature to convert? \n Enter Temperature:"));
+var temp = prompt ("What is the temperature to convert? \n Enter Temperature:");
 var cel;
 var far;
 
 if (degree = "c") {
 	cel =  (temp - 32) * 5/9;
 	console.log(cel);
+	alert(cel);
 
 }else if (degree = "f") {
-	far = temp * 9/5 + 32;
+	far = (temp * 9)/5 + 32;
 	console.log(far);
 }
