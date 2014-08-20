4e126075223df887c5c9ebd575cabb5ee6815c82
twashi28 <terencewashington@Terences-MacBook-Pro.local>
2014-08-20 07:02:33 -0400

    Multiple changes
    Changed Number() to parseInt(). Changed the || operator to &&. Added
    the next grade range calculation.

--- Conditionals_worksheet/Group 2/js/script.js
+++ Conditionals_worksheet/Group 2/js/script.js
@@ -5,11 +5,16 @@
  */
 
 // set the prompt to input a grade.
-var sPercent = Number(prompt("Enter your grade", 90));
+var sPercent = parseInt(prompt("Enter your grade", 90));
 var sGrade;
 
 // calculating the first range of grade
-if (sPercent == 100 || sPercent >= 90) {
-	alert("You have a " + sPercent + "%, which means you have earned a(n) " + sGrade + " in the class!");
-	console.log("You have a");
+if (sPercent <= 100 && sPercent >= 95) {
+	sGrade = "A+";
+	alert("You have " + sPercent + "%, which means you have earned a(n) " + sGrade + " in the class!");
+	console.log("You have a " + sPercent + "%, which means you have earned a(n) " + sGrade + " in the class!");
+}else if (sPercent <= 94 && sPercent >= 90) {
+	sGrade = "A";
+	alert("You have " + sPercent + "%, which means you have earned a(n) " + sGrade + " in the class!");
+	console.log("You have a " + sPercent + "%, which means you have earned a(n) " + sGrade + " in the class!");
 }
