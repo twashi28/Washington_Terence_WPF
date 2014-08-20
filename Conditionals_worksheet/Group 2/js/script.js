21a71ecd333600be0e95fe91deb676d8b13fd277
twashi28 <terencewashington@Terences-MacBook-Pro.local>
2014-08-20 06:21:04 -0400

    Logical Operators
    changed the && operator to || and now I’m getting a print to the
    console.

--- Conditionals_worksheet/Group 2/js/script.js
+++ Conditionals_worksheet/Group 2/js/script.js
@@ -5,11 +5,11 @@
  */
 
 // set the prompt to input a grade.
-var grade = Number(prompt("Enter your grade", 90));
-var letter;
+var sPercent = Number(prompt("Enter your grade", 90));
+var sGrade;
 
 // calculating the first range of grade
-if (grade == 100 && grade >= 90) {
-	letter = "A"
-	alert("You have a " + grade + "%, which means you have earned a(n) " +  + "in the class!");
-};
+if (sPercent == 100 || sPercent >= 90) {
+	alert("You have a " + sPercent + "%, which means you have earned a(n) " + sGrade + " in the class!");
+	console.log("You have a");
+}
