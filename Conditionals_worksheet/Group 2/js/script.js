aab6c4e0aa18118cb9c4a906179b54cdfed6d29c
twashi28 <terencewashington@Terences-MacBook-Pro.local>
2014-08-20 01:02:27 -0400

    Testing first grade calc
    100 to 90 should result in an A

--- Conditionals_worksheet/Group 2/js/script.js
+++ Conditionals_worksheet/Group 2/js/script.js
@@ -1,13 +1,15 @@
 /* Terence Washington
  * WPF Section 01
- * Celsius to Fahrenheit converter
+ * Grade Letter Calculator
  * 8-19-2014
  */
 
-var age = 6;
-var book;
+// set the prompt to input a grade.
+var grade = Number(prompt("Enter your grade", 90));
+var letter;
 
-// if the child is under 10, they get green eggs and ham, otherwise they get the time machine
-
-book = (age < 10) ? "Green Eggs and Ham" : "Time Machine";
-console.log(book);
+// calculating the first range of grade
+if (grade == 100 && grade >= 90) {
+	letter = "A"
+	alert("You have a " + grade + "%, which means you have earned a(n) " +  + "in the class!");
+};
