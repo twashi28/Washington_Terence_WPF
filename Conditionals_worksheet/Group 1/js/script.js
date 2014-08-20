44f885c52390bf3428b9f9150130f97d37ecb14e
twashi28 <terencewashington@Terences-MacBook-Pro.local>
2014-08-19 22:10:04 -0400

    First test
    run test 1

--- Conditionals_worksheet/Group 1/js/script.js
+++ Conditionals_worksheet/Group 1/js/script.js
@@ -7,12 +7,14 @@
 
 // Setting the variables and prompts
 var degree = prompt ("What degree would you like to convert to \n C or F:", 20);
-var temp = prompt ("What is the temperature to convert? \n Enter emperature:")
+var temp = prompt ("What is the temperature to convert? \n Enter Temperature:")
 var cel;
 
 if (degree = "c") {
-	temp - 32 * 5/9
+	temp - 32 * 5/9;
+	console.log(temp);
 
 }else if (degree = "f") {
-	temp * 9/5 + 32
+	temp * 9/5 + 32;
+	console.log(temp);
 }
