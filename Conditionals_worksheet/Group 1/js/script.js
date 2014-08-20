e3f6293dd66c68519873c289b81d9a366fab2eaf
twashi28 <terencewashington@Terences-MacBook-Pro.local>
2014-08-19 22:05:56 -0400

    Setting up group one
    Added folders, Setting up variables and calculations for group one

---
+++ Conditionals_worksheet/Group 1/js/script.js
@@ -0,0 +1,18 @@
+/* Terence Washington
+ * WPF Section 01
+ * Celsius to Fahrenheit converter
+ * 8-19-2014
+ */
+
+
+// Setting the variables and prompts
+var degree = prompt ("What degree would you like to convert to \n C or F:", 20);
+var temp = prompt ("What is the temperature to convert? \n Enter emperature:")
+var cel;
+
+if (degree = "c") {
+	temp - 32 * 5/9
+
+}else if (degree = "f") {
+	temp * 9/5 + 32
+}
---
+++ Conditionals_worksheet/Group 2/js/script.js
@@ -0,0 +1,13 @@
+/* Terence Washington
+ * Ternary Operators
+ */
+
+
+
+var age = 6;
+var book;
+
+// if the child is under 10, they get green eggs and ham, otherwise they get the time machine
+
+book = (age < 10) ? "Green Eggs and Ham" : "Time Machine";
+console.log(book);
