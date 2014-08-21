febef54a9894ccf9f471dbbfdf842d33c10af9cb
twashi28 <terencewashington@Terences-MacBook-Pro.local>
2014-08-21 06:43:58 -0400

    Changes
    modified the validate statements

--- Assignment Conditionals/Conditionals_Personal/js/script.js
+++ Assignment Conditionals/Conditionals_Personal/js/script.js
@@ -8,14 +8,14 @@
 var hrWork = prompt("Enter your hours of work \n Work hours:", 8);
 
 //Re-prompt if age is empty
-if (hrWork === "" || hrWork === "0") {
+if (hrWork === "") {
 	hrWork = prompt("Please enter your hours of work");
 }
 
 var hrSleep = prompt("Enter your sleep hours \n Sleep hours:", 8);
 
 // Re-prompt if time is empty
-if (hrSleep === "" || hrSleep === "0") {
+if (hrSleep === "") {
 	hrSleep = prompt("Please enter your hours of sleep");
 }
 
@@ -27,4 +27,4 @@ var result2 = "You need a vacation.";
 
 
 //Set ternary statment and alerts the results
-time = (freeTime > usedTime) ? alert(result1); : alert(result2);
+time = (freeTime >= usedTime) ? alert(result1) : alert(result2);
