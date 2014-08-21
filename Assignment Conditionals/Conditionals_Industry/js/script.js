e690e1beb108eb24b64263977537aa6065b91bd1
twashi28 <terencewashington@Terences-MacBook-Pro.local>
2014-08-21 03:36:24 -0400

    Conditional assignments
    Conditional assignment folders and files

---
+++ Assignment Conditionals/Conditionals_Industry/js/script.js
@@ -0,0 +1,34 @@
+/* Terence Washington
+ * WPF Section 01
+ * Movie Ticket Price
+ * 8-21-2014
+ */
+
+// Setting variables
+var age = prompt("Enter your age \n Age:", 17);
+
+//Re-prompt if age is empty
+if (age === "" || age === "0") {
+	age = prompt("Please enter your age");
+}
+
+var time = prompt("What time does the movie start? \n Start Time:", 7);
+
+// Re-prompt if time is empty
+if (time === "" || time === "0") {
+	time = prompt("Please enter movie time");
+}
+
+var price;
+
+
+//Set ternary statment
+price = ((age >= 55 || age <= 10) || (time >= 3 && time <= 5)) ? 7 : 12;
+
+//Alerts the result of discount price
+	alert("The ticket price is $" + price);
+	
+	//Prints to the console of discount price
+	console.log("The ticket price is $" + price);
+	console.log("Age: " + age);
+	console.log("Time: " + time);
