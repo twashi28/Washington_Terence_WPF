91adf495b41ee1158d087f51dd7dd2b7782facce
twashi28 <terencewashington@Terences-MacBook-Pro.local>
2014-08-21 01:35:22 -0400

    Second test
    added second condition within the if statements

--- Conditionals_worksheet/Group 3/js/script.js
+++ Conditionals_worksheet/Group 3/js/script.js
@@ -10,10 +10,16 @@ var time = prompt("What time does the movie start? \n Start Time:", 7);
 var price;
 
 //If statments for calculation of discount prices
-if (age >= 55 || age <= 10){
+if ((age >= 55 || age <= 10) && (time >= 3 || time <= 5)){
 	price = 7;
 	alert("The ticket price is $" + price);
 	console.log("The ticket price is $" + price);
-	console.log(age);
-	console.log(time);
+	console.log("Age: " + age);
+	console.log("Time: " + time);
+}else {
+	price = 12;
+	alert("The ticket price is $" + price);
+	console.log("The ticket price is $" + price);
+	console.log("Age: " + age);
+	console.log("Time: " + time);
 }
