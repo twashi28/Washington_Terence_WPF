18433cbbada30310bf1457eb7357091299581b9d
twashi28 <terencewashington@Terences-MacBook-Pro.local>
2014-08-21 01:30:04 -0400

    input/output test
    testing the prompts and first calculations

--- Conditionals_worksheet/Group 3/js/script.js
+++ Conditionals_worksheet/Group 3/js/script.js
@@ -4,7 +4,16 @@
  * 8-21-2014
  */
 
-
+// Setting variables
 var age = prompt("Enter your age \n Age:", 17);
 var time = prompt("What time does the movie start? \n Start Time:", 7);
 var price;
+
+//If statments for calculation of discount prices
+if (age >= 55 || age <= 10){
+	price = 7;
+	alert("The ticket price is $" + price);
+	console.log("The ticket price is $" + price);
+	console.log(age);
+	console.log(time);
+}
