47723ebe411092054019f4b02f3371881ebea514
twashi28 <terencewashington@Terences-MacBook-Pro.local>
2014-08-21 23:34:03 -0400

    Small changes
    added comments. edited the outputs.

--- Assignment Conditionals/Conditionals_Wacky/js/script.js
+++ Assignment Conditionals/Conditionals_Wacky/js/script.js
@@ -5,23 +5,31 @@
  */
 
 // Setting the array var to hold some items
-var snack = ["chips", "brownie", "gummies", "pretzel"];
+var snack = ["chips", "brownies", "gummies", "pretzels"];
 snack [0];
 snack[1];
 snack[2];
 snack[3];
 
 //Setting a prompt to collect info from the user
-var uSnack = prompt ("What snack would you like to eat? \n chips, brownie, gummies, or pretzel");
+var uSnack = prompt ("What snack would you like to eat? \n chips, brownies, gummies, or pretzels");
 
+// This validates the input form the user
 if (uSnack === "") {
 	uSnack = prompt("Please enter a snack");
 }
+//Setting a prompt to collect info from the user
+var hMany = Number(prompt ("How many do you want?"));
 
-var hMany = prompt ("How many do you want?");
 
+// This validates the input form the user
 if (hMany === "") {
-	hMany = prompt("Please the amount you want");
+	hMany = Number(prompt("Please the amount you want"));
 }
 
+// Increments the amount the user wants by the same number
+hMany +=hMany;
 
+
+// Set the print out from alert and console
+alert("So you like " + uSnack + "? You can have " + hMany + " more and some " + snack[1]".");
