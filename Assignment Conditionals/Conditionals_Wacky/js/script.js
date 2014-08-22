a844d0816bdfb329c21ef287cacc8cfd1869bae3
twashi28 <terencewashington@Terences-MacBook-Pro.local>
2014-08-21 23:16:52 -0400

    Validate prompts and array
    added prompt checks and set array

--- Assignment Conditionals/Conditionals_Wacky/js/script.js
+++ Assignment Conditionals/Conditionals_Wacky/js/script.js
@@ -5,8 +5,23 @@
  */
 
 // Setting the array var to hold some items
-var snack = [chips, brownie, gummies, pretzel];
+var snack = ["chips", "brownie", "gummies", "pretzel"];
+snack [0];
+snack[1];
+snack[2];
+snack[3];
 
 //Setting a prompt to collect info from the user
-var uSnack = prompt("What snack would you like to eat? \n chips, brownie, gummies, or pretzel", chips);
+var uSnack = prompt ("What snack would you like to eat? \n chips, brownie, gummies, or pretzel");
+
+if (uSnack === "") {
+	uSnack = prompt("Please enter a snack");
+}
+
+var hMany = prompt ("How many do you want?");
+
+if (hMany === "") {
+	hMany = prompt("Please the amount you want");
+}
+
 
