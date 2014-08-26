dfb92929fca563e5a6cb1dd5b8bee409f16eddc0
twashi28 <terencewashington@Terences-MacBook-Pro.local>
2014-08-26 06:14:04 -0400

    Arguments and Parameters
    A and P

---
+++ Functions/Arguments and Parameters/js/script.js
@@ -0,0 +1,21 @@
+/* Terence Washington
+ * Function Invocation
+ */
+
+function outptMsg () {
+  console.log("Hello World");
+  
+}
+
+//Sets up the function
+function calcArea () {
+	var width = 20;
+	var height = 30;
+	var area = width * height;
+  console.log(area);
+}
+
+// Calling the function
+calcArea();
+calcArea();
+calcArea();
--- Functions/Variable Scope/js/script.js
+++ Functions/Variable Scope/js/script.js
@@ -7,11 +7,11 @@ function outptMsg () {
   
 }
 
-var width = 5;
+var width = 5; // this var is scoped to the entire document
 
 //Sets up the function
 function calcArea () {
-	var width = 20; //the output is used because this variable is within the function
+	var width = 20; //the output is used because this variable is within the function, scoped to the function
 	var height = 30;
 	var area = width * height;
   console.log(area);
