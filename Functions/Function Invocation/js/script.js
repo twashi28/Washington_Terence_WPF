c6217027675765cd56caff4ed1afee2f09b0309b
twashi28 <terencewashington@Terences-MacBook-Pro.local>
2014-08-26 06:03:18 -0400

    Calling the function.
    toe the name to call the function

---
+++ Functions/Function Invocation/js/script.js
@@ -0,0 +1,18 @@
+/* Terence Washington
+ * Function Invocation
+ */
+
+function outptMsg () {
+  console.log("Hello World");
+  
+}
+
+function calcArea () {
+	var width = 20;
+	var height = 30;
+	var area = width * height;
+  console.log(area);
+}
+
+// Calling the function
+calcArea();
