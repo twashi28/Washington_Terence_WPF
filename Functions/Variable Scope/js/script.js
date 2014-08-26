6fdac5e805e216fd782253588f7d4fd5c9f1e356
twashi28 <terencewashington@Terences-MacBook-Pro.local>
2014-08-26 06:11:49 -0400

    Variable Scope
    The calc output is using the vars that are inside its function. the
    console log is using the var that is outside the function because of
    the matching scope

---
+++ Functions/Variable Scope/js/script.js
@@ -0,0 +1,25 @@
+/* Terence Washington
+ * Variable Scope
+ */
+
+function outptMsg () {
+  console.log("Hello World");
+  
+}
+
+var width = 5;
+
+//Sets up the function
+function calcArea () {
+	var width = 20; //the output is used because this variable is within the function
+	var height = 30;
+	var area = width * height;
+  console.log(area);
+}
+
+// Calling the function
+calcArea(); //using the vars and calcs inside the named function
+calcArea();
+calcArea();
+
+console.log(width); // using the var that is outside with the matching scope distance
