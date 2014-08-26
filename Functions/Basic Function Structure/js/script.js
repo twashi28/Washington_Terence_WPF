f214dc00ddf26fee04febe9553bb0db6394ac684
twashi28 <terencewashington@Terences-MacBook-Pro.local>
2014-08-26 05:45:58 -0400

    Functions
    The Normal Basic Function Structure

---
+++ Functions/Basic Function Structure/js/script.js
@@ -0,0 +1,16 @@
+/* Terence Washington
+ * If and else
+ */
+
+var kidHeight = 50;
+var minHeight = 48;
+
+// If the child is over 48 in height
+
+if(kidHeight > minHeight){
+	// code performed if condition is true
+	console.log("You can ride the coaster.");
+}else{
+	// code performed if condition is false
+	console.log("You are too short for this ride.");
+}
