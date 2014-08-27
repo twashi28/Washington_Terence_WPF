467f20a374984324f2957ca16fb6c7b516b5d441
twashi28 <terencewashington@Terences-MacBook-Pro.local>
2014-08-27 19:41:47 -0400

    Parameter
    Added a parameter in the function

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -5,10 +5,9 @@
  */
 
 
-var cirCumf = function (r) {      //Set a variable as a function with a parameter inside
-	var p = 3.14;               //Setting a variable for pi 3.14
-  var cumf = (2 * r) * p;         //Setting a variable for the calculation of circumference
+var cirCumf = function (r, pi) {      //Set a variable as a function with a parameter inside
+  var cumf = (2 * r) * pi;         //Setting a variable for the calculation of circumference
   return cumf;                  // Returning the calculated answer out of the function
 }
-var answer = cirCumf(2);       //Setting a variable to call the function with arguments
+var answer = cirCumf(2, 3.14);       //Setting a variable to call the function with arguments, Result variable
 console.log("The circumference of the circle is " + answer + ".");           //Printing the answer to the console
