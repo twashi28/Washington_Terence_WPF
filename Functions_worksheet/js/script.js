a5d81793bf566d0a08a43ead7899de1978a55067
twashi28 <terencewashington@Terences-MacBook-Pro.local>
2014-08-27 19:55:18 -0400

    Stung
    Completed second problem of the worksheet.

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -11,3 +11,11 @@ var cirCumf = function (r, pi) {      //Set a variable as a function with a para
 }
 var answer = cirCumf(2, 3.14);       //Setting a variable to call the function with arguments, Result variable
 console.log("The circumference of the circle is " + answer + ".");           //Printing the answer to the console
+
+
+var beeSting = function (w, s) {      //Set a variable as a function with a parameter inside
+  var stings = w * s;         //Setting a variable for the calculation of circumference
+  return stings;                  //Returning the calculated answer out of the function
+}
+var answer2 = beeSting(10, 8.666666667);    //Setting a variable to call the function with arguments, Result variable
+console.log("It takes " + answer2 + " bee stings to kill this animal.")      //Printing the answer to the console
