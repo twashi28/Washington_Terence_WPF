2b11fc30921f23dd07b534abc58fd0fd4a6c4979
twashi28 <terencewashington@Terences-MacBook-Pro.local>
2014-08-27 20:23:09 -0400

    Loops
    while and do while

---
+++ Loops/Do While Loop/js/script.js
@@ -0,0 +1,13 @@
+/* Terence Washington
+ * Do While Loop
+ * 8-26-2014
+ */
+
+
+var a = 10;  
+
+do {
+	console.log(a + " times more.");
+	a--;
+}
+while (a > 0);
---
+++ Loops/While Loop/js/script.js
@@ -0,0 +1,12 @@
+/* Terence Washington
+ * While Loop
+ * 8-26-2014
+ */
+
+
+var a = 10;     // set up the index
+
+while (a > 0){   //checks the condition
+	console.log(a + " More times.");
+	a--;     // increments or decrements the index
+}
