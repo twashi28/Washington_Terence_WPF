c6d413a0bfc0bf6cef14284e535dbae73d652f77
twashi28 <terencewashington@Terences-MacBook-Pro.local>
2014-08-28 23:21:17 -0400

    Functions wacky
    Working on the wacky functions

--- Assignment Functions/Functions_Wacky/js/script.js
+++ Assignment Functions/Functions_Wacky/js/script.js
@@ -1,36 +1,38 @@
 /* Terence Washington
  * WPF Section 01
- * Conditional Wacky
- * 8-21-2014
+ * Function Wacky
+ * 8-28-2014
  */
 
-// Setting the array var to hold some items
-var snack = ["chips", "brownies", "gummies", "pretzels"];
-snack [0];
-snack[1];
-snack[2];
-snack[3];
-snack[4] = "cake";
+//  Setting prompts as variables
+var ram = prompt("Place a number in here.");
 
-//Setting a prompt to collect info from the user
-var uSnack = prompt ("What snack would you like to eat? \n chips, brownies, gummies, or pretzels");
-
-// This validates the input form the user
-if (uSnack === "") {
-	uSnack = prompt("Please enter a snack");
+if (ram === "") {
+	ram = Number(prompt("Please enter a number to start"));
 }
-//Setting a prompt to collect info from the user
-var hMany = Number(prompt ("How many do you want?"));
+var sic = prompt("Place another number please.");
 
+if (sic === "") {
+	sic = Number(prompt("Please enter another to begin"));
+}
+var per = prompt("Place a third digit here.");
 
-// This validates the input form the user
-if (hMany === "") {
-	hMany = Number(prompt("Please the amount you want"));
+if (per === "") {
+	per = Number(prompt("Please enter one more number"));
 }
 
-// Increments the amount the user wants by the same number
-hMany +=hMany;
 
 
-// Set the print out from alert and console
-alert("So you like " + uSnack + "? You can have " + hMany + " more and some " + snack[4] + ".");
+//Set a ternary statement for the second prompt value
+var wackOut = ( sic > 20) ? "Not Wacky Enough" : "Too Wacky.";
+	console.log(wackOut);
+
+//A function with three parameters for the calculations
+var calcWacky = function(r, s, p){
+	var wack = r * s - p *p /r + s;
+	return wack; // Returns the result
+}
+
+// setting a result variable to print out through the console
+var answer = calcWacky(ram, sic, per);
+console.log("This wacky calculation came to about " + answer + ". Makes sense...");
